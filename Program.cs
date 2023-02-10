6a452917f20751a134a1f78d39174d8a4e3e97f6
L_E_S_H_I_I <86139357+LazyLeshii@users.noreply.github.com>
2023-02-10 22:33:53 +0600

    Задание
    Jose set up a circular poker table for his friends so that each of the seats at the table has the same number of poker chips.
    But when Jose wasn’t looking, someone rearranged all of the chips so that they are no longer evenly distributed!
    Now Jose needs to redistribute the chips so that every seat has the same number before his friends arrive.
    But Jose is very meticulous: to ensure that he doesn’t lose any chips in the process, he only moves chips between adjacent seats.
    Moreover, he only moves chips one at a time. What is the minimum number of chip moves
    Jose will need to make to bring the chips back to equilibrium?
    Input:
     
    chips: [1, 5, 9, 10, 5]
     
     
    Expected Output:
     
    12

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+
+namespace app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 5, 9, 10, 5 };
+            int size = array.Sum() / array.Length;
+            int counter = 0;
+
+            while (array.Max() != size)
+            {
+                int index = Array.IndexOf(array, array.Max());
+                int after = index - 1;
+                int before = (index + 1) % array.Length;
+                array[array[after] > array[before] ? before : after] += array[index] - size;
+                counter += array[index] - size;
+                array[index] -= array[index] - size;
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
